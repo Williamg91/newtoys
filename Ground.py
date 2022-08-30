2d3c12bd38f596c6fe27e0bf562e88ceca6b6294
William G <William.geismar@gmail.com>
2022-08-30 13:11:28 +0200

    nu med the boys

--- Ground.py
+++ Ground.py
@@ -1,6 +1,12 @@
 import socket
+import random
 hostName = socket.gethostname()
 HostAddress = socket.gethostbyname(hostName);
 print("My computername is: "+hostName," \n and my adress is "+HostAddress)
 
-print("EH")
+#print("EH")
+
+listlen = random.randint(0,70)
+print (listlen)
+boys = [listlen]
+print (len(boys))
